d4ab635c2dfbddf02079266e771174d6cd1ce17a
noviirna <novi.irnawati@gmail.com>
2019-07-11 21:31:45 +0700

    Updates

---
+++ precache-manifest.65395c2b4e1a3839c421848099089c23.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "108e3d351d4ba1e33c4b2efb8f671c0d",
+    "url": "/react-asyik-banget/index.html"
+  },
+  {
+    "revision": "8d408604a2202478b432",
+    "url": "/react-asyik-banget/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "5eb8ea12aa97ecace870",
+    "url": "/react-asyik-banget/static/js/2.f8412b95.chunk.js"
+  },
+  {
+    "revision": "8d408604a2202478b432",
+    "url": "/react-asyik-banget/static/js/main.38629caf.chunk.js"
+  },
+  {
+    "revision": "e6f452691147aadbd8ca",
+    "url": "/react-asyik-banget/static/js/runtime~main.bc220e89.js"
+  }
+]);
